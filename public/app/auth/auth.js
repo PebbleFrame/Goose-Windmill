168fb889c95e75ae0fd5bf6d05dcba7c88e2cb51
Jeff Peoples <meisterpeeps@gmail.com>
2015-05-15 10:02:20 -0700

    Username is displayed on login. Following switched to dashboard

--- public/app/auth/auth.js
+++ public/app/auth/auth.js
@@ -2,10 +2,13 @@ angular.module('hack.auth', [])
 
 .controller('AuthController', ["$scope", "$window", "$location", "Auth", "Followers", 
   function ($scope, $window, $location, Auth, Followers) {
-  
+  $scope.username = null;
   $scope.user = {};
   $scope.newUser = {};
   $scope.loggedIn = Auth.isAuth();
+  if($scope.loggedIn){
+    $scope.username = $window.localStorage.getItem('hfUser');
+  };
   $scope.badLogin = false;
   $scope.badLoginMessage = '';
   $scope.badSignup = false;
@@ -20,11 +23,13 @@ angular.module('hack.auth', [])
         $scope.badSignupMessage = '';
         $window.localStorage.setItem('com.hack', data.token);
         $window.localStorage.setItem('hfUsers', data.followers)
+        $window.localStorage.setItem('hfUser', Auth.getUser())
 
         Followers.init(data.followers.split(","));
 
         $scope.loggedIn = true;
         $scope.user = {};
+        $scope.username = Auth.getUser()
       })
       .catch(function (error) {
         $scope.badSignup = false;
@@ -45,9 +50,11 @@ angular.module('hack.auth', [])
         $scope.badSignup = false;
         $scope.badSignupMessage = '';
         $window.localStorage.setItem('com.hack', data.token);
+        $window.localStorage.setItem('hfUser', Auth.getUser());
 
         $scope.loggedIn = true;
         $scope.newUser = {};
+        $scope.username = Auth.getUser()
       })
       .catch(function (error) {
         $scope.badLogin = false;
@@ -62,5 +69,6 @@ angular.module('hack.auth', [])
     Followers.init();
     Auth.signout();
     $scope.loggedIn = false;
+    $scope.username = null;
   }
 }]);
